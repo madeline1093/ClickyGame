85ae0bbb2192993cf57a1cdb6d70fd70340c1c15
Madeline Story <madeline1093@gmail.com>
2018-06-27 15:40:03 -0500

    hmm

---
+++ src/components/Container.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Container extends React.Component {
+    state ={
+        role: "img",
+        class: "click-item",
+        style: "background-image"
+    };
+}
+
+componentDidMount() {
+
+}
+
+displayRandomImage() {
+    
+}
